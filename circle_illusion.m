49035cb41fb6133d2b3477c2bdc3e7018f57011c
André Mendes <asmgma@gmail.com>
2021-04-20 00:20:57 -0300

    Add files via upload

---
+++ circle_illusion.m
@@ -0,0 +1,52 @@
+%% Circle Illusion - Moving dots to rotating circle
+% Animation of a rotating circle that emerges from a set of dots moving
+% harmonically in straight lines with different orientations.
+%
+%%
+
+clear ; close all ; clc
+
+%% Parameters
+
+tf  = 30;                       % Final time                [s]
+dt  = 30;                       % Frame rate                [fps]
+t   = linspace(0,tf,tf*dt);     % Time                      [s]
+T   = 10;                       % Oscillation period        [s]
+f   = 1/T;                      % Oscillation frequency     [Hz]
+A   = 1;                        % Amplitude                 [m]
+N   = 8;                        % Number of dots
+ph  = pi/N;                     % Phase                     [rad]
+th  = pi/N;                     % Orientation               [rad]
+
+%% Defining motion
+
+% Preallocating
+px = zeros(length(t),N);
+py = zeros(length(t),N);
+
+% Position matrices
+for i = 1:N
+    px(:,i) = A*sin(2*pi*f*t + i*ph) * cos(i*th);
+    py(:,i) = A*sin(2*pi*f*t + i*ph) * sin(i*th);
+end
+
+%% Animation
+
+figure
+hold on ; grid on ; axis equal
+set(gca,'xlim',[-1.1 1.1],'ylim',[-1.1 1.1])
+
+% Create and open video writer object
+v = VideoWriter('circle_illusion.avi');
+v.Quality = 100;
+open(v);
+
+% Generating frames
+for i=1:length(t)
+    cla
+    plot(px(i,:),py(i,:),'ro')
+    frame = getframe(gcf);
+    writeVideo(v,frame);
+end
+
+close(v);
