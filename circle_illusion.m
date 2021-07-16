b519085d8917bb7e1db6da556242eb957f2ca5f8
André de Souza Mendes <asmendes@fei.edu.br>
2021-07-16 15:33:51 -0300

    Update circle_illusion.m

--- circle_illusion.m
+++ circle_illusion.m
@@ -37,8 +37,9 @@ end
 %% Animation
 
 figure
-% set(gcf,'Position',[50 50 1280 720]) % 720p
-% set(gcf,'Position',[50 50 854 480]) % 480p
+% set(gcf,'Position',[50 50 1280 720]) % YouTube: 720p
+% set(gcf,'Position',[50 50 854 480]) % YouTube: 480p
+set(gcf,'Position',[50 50 640 640]) % Instagram
 
 hold on ; grid on ; box on ; axis equal
 set(gca,'xlim',[-1.1 1.1],'ylim',[-1.1 1.1])
